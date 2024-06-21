a6570634df8649ccb421e1f4dc140c3407477154
pablomoko <pablomokorel@gmail.com>
2024-06-21 19:41:01 -0300

    Se agrego zanahoria

--- server_src/pickups/zanahoria_envenenada.cpp
+++ server_src/pickups/zanahoria_envenenada.cpp
@@ -2,21 +2,12 @@
 
 ZanahoriaEnvenenada::ZanahoriaEnvenenada(float x, float y, std::map<std::string, float>& config,
                                          Queue<Container>& q):
-        Pickup(x, y, 3, 5, EntityType::CARROT, AnimationType::PICKUP, q),
+        Pickup(x, y, 3, 5, EntityType::INTOXICATED_CARROT, AnimationType::PICKUP, q),
         config(config),
         life(config["carrot_addlife"]) {}
 
 void ZanahoriaEnvenenada::colision(Personaje& p) {
     borrar = true;
-    if (p.vida > 0) {
-        p.vida += life;
-    }
-
-    if (p.vida > 100) {
-        p.vida = config["player_life"];
-    }
-
-    std::cout << "Vida: " << p.vida << "\n";
 
     p.colision(*this);
 
